fb22977b26c5955299362d1ce37359751c588834
jingxiaogu1216 <jingxiao1216@gmail.com>
2016-11-10 15:42:51 -0500

    finish backend

--- webserver/server.py
+++ webserver/server.py
@@ -1,233 +1,294 @@
-#!/usr/bin/env python2.7
-
-"""
-Columbia W4111 Intro to databases
-Example webserver
-
-To run locally
-
-    python server.py
-
-Go to http://localhost:8111 in your browser
-
-
-A debugger such as "pdb" may be helpful for debugging.
-Read about it online.
-"""
-
 import os
+import time
+from flask import Flask, request, render_template, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import *
-from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from sqlalchemy.exc import *
+import uuid
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
 
-#
-# The following uses the postgresql test.db -- you can use this for debugging purposes
-# However for the project you will need to connect to your Part 2 database in order to use the
-# data
-#
-# XXX: The URI should be in the format of: 
-#
-#     postgresql://USER:PASSWORD@<IP_OF_POSTGRE_SQL_SERVER>/postgres
-#
-# For example, if you had username ewu2493, password foobar, then the following line would be:
-#
-#     DATABASEURI = "postgresql://ewu2493:foobar@<IP_OF_POSTGRE_SQL_SERVER>/postgres"
-#
-# Swap out the URI below with the URI for the database created in part 2
-DATABASEURI = "sqlite:///test.db"
-
-
-#
-# This line creates a database engine that knows how to connect to the URI above
-#
-engine = create_engine(DATABASEURI)
-
-
-#
-# START SQLITE SETUP CODE
-#
-# after these statements run, you should see a file test.db in your webserver/ directory
-# this is a sqlite database that you can query like psql typing in the shell command line:
-# 
-#     sqlite3 test.db
-#
-# The following sqlite3 commands may be useful:
-# 
-#     .tables               -- will list the tables in the database
-#     .schema <tablename>   -- print CREATE TABLE statement for table
-# 
-# The setup code should be deleted once you switch to using the Part 2 postgresql database
-#
-engine.execute("""DROP TABLE IF EXISTS test;""")
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
-#
-# END SQLITE SETUP CODE
-#
+database_url = ''
+engine = create_engine(database_url)
+conn = engine.connect()
 
 
 
-@app.before_request
-def before_request():
-  """
-  This function is run at the beginning of every web request 
-  (every time you enter an address in the web browser).
-  We use it to setup a database connection that can be used throughout the request
-
-  The variable g is globally accessible
-  """
-  try:
-    g.conn = engine.connect()
-  except:
-    print "uh oh, problem connecting to database"
-    import traceback; traceback.print_exc()
-    g.conn = None
-
-@app.teardown_request
-def teardown_request(exception):
-  """
-  At the end of the web request, this makes sure to close the database connection.
-  If you don't the database could run out of memory!
-  """
-  try:
-    g.conn.close()
-  except Exception as e:
-    pass
 
 
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to e.g., localhost:8111/foobar/ with POST or GET then you could use
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
-@app.route('/')
-def index():
-  """
-  request is a special object that Flask provides to access web request information:
-
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
-
-  # DEBUG: this is debugging code to see what request looks like
-  print request.args
+@app.route('/', methods=['GET'])
+def signup_page():
+	return render_template('signup.html')
 
 
-  #
-  # example of a database query
-  #
-  cursor = g.conn.execute("SELECT name FROM test")
-  names = []
-  for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
-  cursor.close()
-
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = names)
+@app.route('/signup', methods=['POST'])
+def signup():
+	user_id = uuid.uuid4()
+	name = request.form['name']
+	email = request.form['email']
+	driver_license = request.form['driver_license']
+	dob = request.form['dob']
+	password_hashing = generate_password_hash(request.form['password'])
+	try:
+		conn.execute("insert into users(user_id, name, email, password, driver_license, birth) values(%s, %s, %s, %s, %s, %s)", user_id, name, email, password_hashing, driver_license, dob)
+		return "success"
+	except IntegrityError:
+		return "email already exists"
+	except Exception as e:
+		print e.message
+		return "fail"
 
 
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html", **context)
-
-#
-# This is an example of a different path.  You can see it at
-# 
-#     localhost:8111/another
-#
-# notice that the functio name is another() rather than index()
-# the functions for each app.route needs to have different names
-#
-@app.route('/another')
-def another():
-  return render_template("anotherfile.html")
-
-
-# Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-  name = request.form['name']
-  print name
-  cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
-  g.conn.execute(text(cmd), name1 = name, name2 = name);
-  return redirect('/')
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+	if request.method == 'GET':
+		return render_template('signin.html')
+	else:
+		email = request.form['email']
+		password = request.form['password']
+		try:
+			cursor = conn.execute("select * from users where email = '%s'" %(email))
+			record = cursor.fetchone()
+			if check_password_hash(record['password'], password):
+				return redirect('/user/' + record.user_id)
+			return "email and password not match"
+		except Exception as e:
+			print e.message
+			return "fail"
 
 
-@app.route('/login')
-def login():
-    abort(401)
-    this_is_never_executed()
+#home page when user successfully log in
+@app.route('/user/<uid>', methods=['GET'])
+def user(uid):
+	try:
+		cursor = conn.execute("select ad_car.ad_id, ad_car.title, ad_car.description, ad_car.location from ad_car")
+		return render_template("user.html", data=cursor.fetchall(), user_id=uid)
+	except Exception as e:
+		print e.message
+		return "fail"
 
 
-if __name__ == "__main__":
-  import click
+# user's profile page.
+@app.route('/user/profile/<uid>', methods=['GET', 'PUT'])
+def user_profile(uid):
+	if request.method == 'GET':
+		try:
+			user_cursor = conn.execute(
+				"select user_id, name, email, driver_license, birth from users where user_id = '%s'" % (uid))
+			print user_cursor.fetchone()
+			return render_template('user_profile.html', data=user_cursor.fetchone())
 
-  @click.command()
-  @click.option('--debug', is_flag=True)
-  @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
-  @click.argument('PORT', default=8111, type=int)
-  def run(debug, threaded, host, port):
-    """
-    This function handles command line parameters.
-    Run the server using
+		except Exception as e:
+			print e.message
+			return "fail"
 
-        python server.py
-
-    Show the help text using
-
-        python server.py --help
-
-    """
-
-    HOST, PORT = host, port
-    print "running on %s:%d" % (HOST, PORT)
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+	# users modify their information
+	else:
+		name = request.form['name']
+		dob = request.form['dob']
+		driver_license = request.form['driver_license']
+		try:
+			conn.execute("update users set name = '%s', birth = '%s', driver_license = '%s' where user_id = '%s'" %(name, dob, driver_license, uid))
+			return "successfully updated"
+		except Exception as e:
+			print e.message
+			return "fail"
 
 
-  run()
+#ad_car page. It's the page where renters can rent and bookmark car ad
+@app.route('/user/<uid>/ad/<ad_id>', methods=['GET', 'POST', 'PUT'])
+def ad_car(uid, ad_id):
+	if request.method == 'GET':
+		try:
+			cursor = conn.execute("select u.name, o.owner_rating, ad.*, from ad_car as ad, users as u, owner as o where ad.ad_id = '%s' and ad.owner_id = u.user_id and ad.owner_id = o.owner_id" %(ad_id))
+			return render_template("ad_car.html", data=cursor.fetchall())
+		except Exception as e:
+			print e.message
+			return "fail"
+
+	# user rent car
+	elif request.method == 'POST':
+		transaction_date = time.strftime("%x")
+		transaction_id = uuid.uuid4()
+		try:
+			owner_id = conn.execute("select owner_id from ad_car where ad_id = '%s'" %(ad_id)).fetchone()['owner_id']
+		except Exception as e:
+			print e.message
+			return "fail"
+
+		# transaction_id, transaction_date, owner_id, renter_id, ad_id, accept, finish
+		try:
+			conn.execute("update ad_car set available = FALSE where ad_id = '%s'" %(ad_id))
+			conn.execute("insert into transaction(transaction_id, transaction_date, owner_id, renter_id, ad_id, accept, finish) values(%s, %s, %s, %s, %s, FALSE, FALSE)", \
+						 transaction_id, transaction_date, owner_id, uid, ad_id)
+
+			cursor = conn.execute("select * from renter where renter_id = '%s'" %(uid))
+			if cursor.rowcount == 0:
+				conn.execute("insert into renter(renter_id, num_of_renting) values (%s, %s)", uid, 1)
+				return "rent success"
+
+			conn.execute("update renter set num_of_renting = num_of_renting + 1 where renter_id = '%s'" %(uid))
+			return "rent success"
+		except Exception as e:
+			print e.message
+			return "fail"
+
+	# user bookmark ad
+	else:
+		try:
+			mark_date = time.strftime("%x")
+			owner_id = conn.execute("select owner_id from ad_car where ad_id = '%s'" %(ad_id)).fetchone()['owner_id']
+			conn.execute("insert into bookmark(renter_id, ad_id, owner_id, mark_date) values(%s, %s, %s, %s)", uid, ad_id, owner_id, mark_date)
+			return "successfully marked"
+		except IntegrityError:
+			return "already bookmarked"
+		except Exception as e:
+			print e.message
+			return "fail"
+
+
+#user's ad car page.
+@app.route('/user/profile/<uid>/ad', methods=['GET', 'POST', 'DELETE'])
+def user_ad_car(uid):
+	if request.method == 'GET':
+		try:
+			cursor = conn.execute("select ad_car.ad_id, ad_car.title, ad_car.description, ad_car.location from ad_car where owner_id = '%s'" %(uid))
+			return render_template("user_ad_car.html", data=cursor.fetchall())
+		except Exception as e:
+			print e.message
+			return "fail"
+
+	# user post car ad
+	elif request.method == 'POST':
+		ad_id = uuid.uuid4()
+		owner_id = uid
+		title = request.form['title']
+		description = request.form['description']
+		location = request.form['location']
+		car_type = request.form['car_type']
+		car_color = request.form['car_color']
+		car_make = request.form['car_make']
+		car_price = request.form['car_price']
+		car_mile = request.form['car_mile']
+		car_plate_num = request.form['car_plate_num']
+		try:
+			conn.execute("insert into ad_car(ad_id, description, title, location, owner_id, plate_number, daily_price, available, type, make, mileages, color) \
+						  values(%s, %s, %s, %s, %s, %s, %s, TRUE, %s, %s, %s, %s)", ad_id, description, title, \
+						  location, owner_id, car_plate_num, car_price, car_type, car_make, car_mile, car_color)
+
+			cursor = conn.execute("select * from owner where owner_id = '%s'" %(uid))
+			if cursor.rowcount == 0:
+				conn.execute("insert into owner(owner_id, owner_rating, owner_rating_num) values(%s, %s, %s)", uid, 3, 0)
+			return "successfully post car ad"
+		except Exception as e:
+			print e.message
+			return "fail"
+
+	# user delete car ad
+	else:
+		ad_id = request.form['ad_id']
+		try:
+			conn.execute("delete from ad_car where ad_id = '%s'" %(ad_id))
+			return "successfully delete car ad"
+		except Exception as e:
+			print e.message
+			return "fail"
+
+
+# user's bookmark page
+@app.route('/user/profile/<uid>/bookmark', methods=['GET', 'PUT'])
+def user_bookmark(uid):
+	# show user's bookmark car ad
+	if request.method == 'GET':
+		try:
+			cursor = conn.execute("select b.mark_date, ad.ad_id, ad.title, ad.description, ad.location from bookmark as b, ad_car as ad where b.renter_id = '%s' and b.ad_id = ad.ad_id" % (uid))
+			print cursor.fetchall()
+			return render_template("user_bookmark.html", data=cursor.fetchall())
+		except Exception as e:
+			print e.message
+			return "fail"
+
+	# user unbookmark car ad
+	else:
+		ad_id = request.form['ad_id']
+		try:
+			conn.execute("delete from bookmark where renter_id = '%s' and ad_id = '%s'" %(uid, ad_id))
+			return "successfully unbookmarked"
+		except Exception as e:
+			print e.message
+			return "fail"
+
+
+# users's transaction page
+@app.route('/user/profile/<uid>/transaction', methods=['GET'])
+def user_transaction(uid):
+	# show user's transaction page
+	try:
+		cursor = conn.execute("select t.*, ad.title, ad.description, ad.location from transaction as t, ad_car as ad where (t.renter_id = '%s' or t.owner_id = '%s') and t.ad_id = ad.ad_id" % (uid, uid))
+		return render_template("user_transaction.html", data=cursor.fetchall())
+	except Exception as e:
+		print e.message
+		return "fail"
+
+
+# owner accept transaction
+@app.route('/user/profile/<uid>/transaction/accept', methods=['PUT'])
+def owner_accept_transaction(uid):
+	ad_id = request.form['ad_id']
+	renter_id = request.form['renter_id']
+	try:
+		conn.execute("update transaction set accept = TRUE where owner_id = '%s' and renter_id = '%s' and ad_id = '%s'" %(uid, renter_id, ad_id))
+		return "successfully accept"
+	except Exception as e:
+		print e.message
+		return "fail"
+
+
+
+# owner finish transaction
+@app.route('/user/profile/<uid>/transaction/finish', methods=['PUT'])
+def owner_finish_transaction(uid):
+	ad_id = request.form['ad_id']
+	renter_id = request.form['renter_id']
+	try:
+		conn.execute("update transaction set finish = TRUE where owner_id = '%s' and renter_id = '%s' and ad_id = '%s'" %(uid, renter_id, ad_id))
+		conn.execute("update ad_car set available = TRUE where ad_id = '%s'" % (ad_id))
+		return "successfully finish transaction"
+	except Exception as e:
+		print e.message
+		return "fail"
+
+
+# renter rate owner
+@app.route('/user/profile/<uid>/transaction/rate', methods=['POST'])
+def renter_rate_owner(uid):
+	comment_id = uuid.uuid4()
+	owner_id = request.form['owner_id']
+	rate = request.form['rate']
+	rate_time = time.strftime("%x")
+	review = request.form['review']
+	try:
+		conn.execute("insert into rate_comment(comment_id, owner_id, renter_id, rate, rate_time, review) values(%s, %s, %s, %s, %s, %s)", comment_id, owner_id, uid, rate, rate_time, review)
+		conn.execute("update owner set owner_rating = (owner_rating * owner_rating_num + '%s') / (owner_rating_num + 1), owner_rating_num = owner_rating_num + 1 where owner_id = '%s'" %(rate, owner_id))
+		return "successfully rated"
+	except Exception as e:
+		print e.message
+		return "fail"
+
+
+# user's comment page
+@app.route('/user/profile/<uid>/comment')
+def user_comment(uid):
+	try:
+		cursor = conn.execute("select u.name as owner_name, ucopy.name as renter_name, rc.* from users as u, users as ucopy, rate_comment as rc \
+							  where (rc.owner_id = '%s' or rc.renter_id = '%s') and rc.owner_id = u.user_id and rc.renter_id = ucopy.user_id" %(uid, uid))
+		return render_template('user_comment.html', cursor.fetchall())
+	except Exception as e:
+		print e.message
+		return "fail"
+
+
+if __name__ == '__main__':
+	app.run()
